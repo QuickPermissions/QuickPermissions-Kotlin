529e6276176b0097a8543f488722eb83dd068ee1
Sebastian Engel <engel.sebastian@gmail.com>
2018-08-04 11:16:06 +0200

    Removing dependency to k4kotlin library.

--- app/src/main/java/com/livinglifetechway/quickpermissions_sample/MainActivity.kt
+++ app/src/main/java/com/livinglifetechway/quickpermissions_sample/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.livinglifetechway.quickpermissions_sample
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.livinglifetechway.k4kotlin.onClick
-import com.livinglifetechway.k4kotlin.setBindingView
 import com.livinglifetechway.quickpermissions_sample.databinding.ActivityMainBinding
 import com.livinglifetechway.quickpermissions_sample.kotlin.AllKotlinActivity
 import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = setBindingView(R.layout.activity_main)
 
-        mBinding.buttonKotlinAll.onClick {
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mBinding.buttonKotlinAll.setOnClickListener {
             startActivity<AllKotlinActivity>()
         }
 
