c004f2939587c5ff72e882a12f84d94b5abf2f24
Kirtan Thakkar <kirtan403@gmail.com>
2018-11-11 15:13:25 +0530

    Updated sample activity to check all the scenarios

--- app/src/main/java/com/livinglifetechway/quickpermissions_sample/kotlin/AllKotlinActivity.kt
+++ app/src/main/java/com/livinglifetechway/quickpermissions_sample/kotlin/AllKotlinActivity.kt
@@ -6,13 +6,21 @@ import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
 import android.widget.Toast
+import com.livinglifetechway.k4kotlin.onClick
+import com.livinglifetechway.k4kotlin.setBindingView
 import com.livinglifetechway.quickpermissions_kotlin.runWithPermissions
 import com.livinglifetechway.quickpermissions_kotlin.util.QuickPermissionsRequest
 import com.livinglifetechway.quickpermissions_sample.R
 import com.livinglifetechway.quickpermissions_kotlin.util.QuickPermissionsOptions
+import com.livinglifetechway.quickpermissions_sample.databinding.ActivityAllKotlinBinding
 
 class AllKotlinActivity : AppCompatActivity() {
 
+    /**
+     * You can hardcode the options like this
+     * However for the sample purpose the options is dynamically generated based on the
+     * values you have selected from the activity
+     */
     private val quickPermissionsOption = QuickPermissionsOptions(
             rationaleMessage = "Custom rational message",
             permanentlyDeniedMessage = "Custom permanently denied message",
@@ -21,14 +29,36 @@ class AllKotlinActivity : AppCompatActivity() {
             permissionsDeniedMethod = { whenPermAreDenied(it) }
     )
 
+    lateinit var mBinding: ActivityAllKotlinBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_all_kotlin)
+        mBinding = setBindingView(R.layout.activity_all_kotlin)
 
-        methodRequiresPermissions()
+        mBinding.button.onClick {
+            // create the quickpermissions object
+            val options = QuickPermissionsOptions()
+            options.handleRationale = mBinding.handleRationale.isChecked
+            if (mBinding.customRationaleMessage.isChecked) {
+                options.rationaleMessage = "Custom rational message"
+            }
+            if (mBinding.customRationaleMethod.isChecked) {
+                options.rationaleMethod = { rationaleCallback(it) }
+            }
+            options.handlePermanentlyDenied = mBinding.handlePermanentlyDenied.isChecked
+            if (mBinding.customPermanentlyDeniedMessage.isChecked) {
+                options.permanentlyDeniedMessage = "Custom permanently denied message"
+            }
+            if (mBinding.customPermanentlyDeniedMethod.isChecked) {
+                options.permanentDeniedMethod = { permissionsPermanentlyDenied(it) }
+            }
+            options.permissionsDeniedMethod = { whenPermAreDenied(it) }
+
+            methodRequiresPermissions(options)
+        }
     }
 
-    private fun methodRequiresPermissions() = runWithPermissions(Manifest.permission.WRITE_CALENDAR, Manifest.permission.RECORD_AUDIO, options = quickPermissionsOption) {
+    private fun methodRequiresPermissions(quickPermissionsOptions: QuickPermissionsOptions) = runWithPermissions(Manifest.permission.WRITE_CALENDAR, Manifest.permission.RECORD_AUDIO, options = quickPermissionsOptions) {
         val toast = Toast.makeText(this, "Cal and microphone permission granted", Toast.LENGTH_LONG)
         toast.setGravity(Gravity.CENTER, 0, 0)
         toast.show()
